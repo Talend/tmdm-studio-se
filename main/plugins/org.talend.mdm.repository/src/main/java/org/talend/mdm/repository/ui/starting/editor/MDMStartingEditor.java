934e54e795b6fa4277da6528c95e5fb90811f57a
cmeng-talend <cmeng@talend.com>
2015-06-04 14:19:40 +0800

    TDI-32047 feature: change the welcome icon of MDM in black background https://jira.talendforge.org/browse/TDI-32047

--- main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/starting/editor/MDMStartingEditor.java
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/starting/editor/MDMStartingEditor.java
@@ -15,8 +15,10 @@ package org.talend.mdm.repository.ui.starting.editor;
 import java.io.IOException;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.browser.Browser;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
@@ -24,14 +26,25 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 import org.talend.commons.ui.html.BrowserDynamicPartLocationListener;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.mdm.repository.plugin.RepositoryPlugin;
 import org.talend.mdm.repository.ui.starting.helper.MDMStartingHelper;
-
 
 public class MDMStartingEditor extends EditorPart {
 
     public static final String ID = "org.talend.mdm.repository.ui.starting.editor.MDMStartingEditor";//$NON-NLS-1$
-    
+
+    protected static final String ICON_WHITE_PATH = "/icons/appli_white_16x16.png"; //$NON-NLS-1$
+
+    protected Image titleImage;
+
     private Browser browser;
+
+    public MDMStartingEditor() {
+        ImageDescriptor imageDescriptor = ImageDescriptor.createFromURL(RepositoryPlugin.class.getResource(ICON_WHITE_PATH));
+        if (imageDescriptor != null) {
+            titleImage = imageDescriptor.createImage();
+        }
+    }
 
     @Override
     public void createPartControl(Composite parent) {
@@ -39,7 +52,7 @@ public class MDMStartingEditor extends EditorPart {
             browser = new Browser(parent, SWT.NONE);
             browser.setText(MDMStartingHelper.getHelper().getHtmlContent());
             browser.addLocationListener(new BrowserDynamicPartLocationListener());
-            
+
             return;
         } catch (IOException e) {
             ExceptionHandler.process(e);
@@ -59,8 +72,9 @@ public class MDMStartingEditor extends EditorPart {
 
     @Override
     public void setFocus() {
-        if(browser != null)
+        if (browser != null) {
             browser.setFocus();
+        }
     }
 
     @Override
@@ -81,5 +95,13 @@ public class MDMStartingEditor extends EditorPart {
     @Override
     public void doSaveAs() {
         // do nothing
+    }
+
+    @Override
+    public Image getTitleImage() {
+        if (titleImage != null) {
+            return titleImage;
+        }
+        return super.getTitleImage();
     }
 }
