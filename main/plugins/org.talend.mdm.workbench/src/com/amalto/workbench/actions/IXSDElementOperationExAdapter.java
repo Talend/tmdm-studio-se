ebc14c0935faaf24b38045fe64eb38a15d0f25bf
hbhong <hbhong@talend.com>
2017-05-16 18:03:53 +0800

    TMDM-10871:Synchronize change context xpath of validation rule in datamodel editor

--- main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/actions/IXSDElementOperationExAdapter.java
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/actions/IXSDElementOperationExAdapter.java
@@ -14,17 +14,15 @@ package com.amalto.workbench.actions;
 
 import java.util.Set;
 
+import org.eclipse.xsd.XSDSchema;
+
 import com.amalto.workbench.exadapter.IExAdapter;
 
 /**
- * created by HHB on 2017-5-9 Detailled comment
+ * created by HHB on 2017-5-15 Detailled comment
  *
  */
-public interface IMatchRuleMapInfoOperationExAdapter<T> extends IExAdapter<T> {
+public interface IXSDElementOperationExAdapter<T> extends IExAdapter<T> {
 
-    void deleteEntityMapInfo(String entityName);
-
-    void renameEntityMapinfo(String oldName, String newName);
-
-    void renameElementMapinfo(Set<String> paths, String newName);
+    void renameElement(XSDSchema schema, Set<String> paths, String newName);
 }
