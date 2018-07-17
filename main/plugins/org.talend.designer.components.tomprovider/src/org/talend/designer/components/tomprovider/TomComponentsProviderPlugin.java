be72c4a8da2e65a22209545b6c8d140ae206ed13
sbliu <sbliu@talend.com>
2018-07-17 15:04:58 +0800

    TMDM-11319 MDM components should not displayed using DI license.

---
+++ main/plugins/org.talend.designer.components.tomprovider/src/org/talend/designer/components/tomprovider/TomComponentsProviderPlugin.java
@@ -0,0 +1,35 @@
+package org.talend.designer.components.tomprovider;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public class TomComponentsProviderPlugin extends AbstractUIPlugin {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+    private static TomComponentsProviderPlugin plugin;
+
+    public TomComponentsProviderPlugin() {
+        plugin = this;
+    }
+
+	@Override
+    public void start(BundleContext bundleContext) throws Exception {
+		TomComponentsProviderPlugin.context = bundleContext;
+	}
+
+	@Override
+    public void stop(BundleContext bundleContext) throws Exception {
+        plugin = null;
+		TomComponentsProviderPlugin.context = null;
+	}
+
+    public static TomComponentsProviderPlugin getDefault() {
+        return plugin;
+    }
+
+}
