4c77cf1c532b1b76202ee56c56c2a7fe52049d68
hbhong <hbhong@talend.com>
2018-10-29 18:19:54 +0800

    TMDM-12686: Fixed always display result in English while switching to other language

---
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/utils/I18nUtil.java
@@ -0,0 +1,65 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package com.amalto.workbench.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.eclipse.core.runtime.Platform;
+
+public class I18nUtil {
+
+    private static final String DEFAULT_NL = "en"; //$NON-NLS-1$
+
+    private static final String OSGI_NL = "osgi.nl"; //$NON-NLS-1$
+
+    private static final Log LOG = LogFactory.getLog(HttpClientUtil.class);
+
+    private static String curNL = null;
+
+    public static final String getCurrentNL() {
+        if (curNL == null) {
+            curNL = readCurrentNL();
+            if (curNL == null || curNL.isEmpty()) {
+                curNL = DEFAULT_NL;
+            }
+        }
+        return curNL;
+    }
+
+    private static String readCurrentNL() {
+        FileInputStream fin = null;
+
+        try {
+            URL url = Platform.getConfigurationLocation().getURL();
+
+            Properties p = new Properties();
+            // load the file configuration/config.ini
+            File iniFile = new File(url.getFile(), "config.ini"); //$NON-NLS-1$
+            fin = new FileInputStream(iniFile);
+            p.load(fin);
+            return p.getProperty(OSGI_NL);
+        } catch (IOException ex) {
+            LOG.error(ex.getMessage(), ex);
+            return null;
+        } finally {
+            IOUtils.closeQuietly(fin);
+        }
+    }
+}
