efc4fdf9bba49237d48f8140f274ccbcbee772fc
sbliu <sbliu@talend.com>
2018-12-17 18:48:54 +0800

    TMDM-12841 refactor.

--- main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/UserVarValueValidator.java
+++ main/plugins/org.talend.mdm.repository/src/main/java/org/talend/mdm/repository/ui/widgets/UserVarValueValidator.java
@@ -7,27 +7,31 @@ public class UserVarValueValidator {
 
     private static final String _PREFIX_USER_VAR = "${user_context."; //$NON-NLS-1$
 
-    private static final String _SURFIX_USER_VAR = "}"; //$NON-NLS-1$
+    private static final String _SUFFIX_USER_VAR = "}"; //$NON-NLS-1$
+
+    private static List<String> validUserVars = new ArrayList<>();
+
+    static {
+        validUserVars.add(_PREFIX_USER_VAR + UserField.Id.field + _SUFFIX_USER_VAR);
+        validUserVars.add(_PREFIX_USER_VAR + UserField.First_Name.field + _SUFFIX_USER_VAR);
+        validUserVars.add(_PREFIX_USER_VAR + UserField.Last_Name.field + _SUFFIX_USER_VAR);
+        validUserVars.add(_PREFIX_USER_VAR + UserField.User_Name.field + _SUFFIX_USER_VAR);
+        validUserVars.add(_PREFIX_USER_VAR + UserField.Language.field + _SUFFIX_USER_VAR);
+    }
 
     /**
      * True: Is valid
      */
     public static boolean validate(String userVarValue) {
-        List<String> validUserVars = new ArrayList<>();
-        validUserVars.add(_PREFIX_USER_VAR + UserField.Id.field + _SURFIX_USER_VAR);
-        validUserVars.add(_PREFIX_USER_VAR + UserField.First_Name.field + _SURFIX_USER_VAR);
-        validUserVars.add(_PREFIX_USER_VAR + UserField.Last_Name.field + _SURFIX_USER_VAR);
-        validUserVars.add(_PREFIX_USER_VAR + UserField.User_Name.field + _SURFIX_USER_VAR);
-        validUserVars.add(_PREFIX_USER_VAR + UserField.Language.field + _SURFIX_USER_VAR);
-
-        String propertyFieldHead = _PREFIX_USER_VAR + UserField.Properties.field + "[\""; //$NON-NLS-1$
-        String propertyFieldTail = "\"]" + _SURFIX_USER_VAR; //$NON-NLS-1$
-
-        boolean isUserVarPattern = userVarValue.startsWith(_PREFIX_USER_VAR) && userVarValue.endsWith(_SURFIX_USER_VAR);
-        boolean propPattern = userVarValue.startsWith(propertyFieldHead) && userVarValue.endsWith(propertyFieldTail)
-                && userVarValue.length() > propertyFieldHead.length() + propertyFieldTail.length();
-        if (isUserVarPattern && !validUserVars.contains(userVarValue) && !propPattern) {
-            return false;
+        boolean isUserVarPattern = userVarValue.startsWith(_PREFIX_USER_VAR) && userVarValue.endsWith(_SUFFIX_USER_VAR);
+        if (isUserVarPattern) {
+            String propertyFieldHead = _PREFIX_USER_VAR + UserField.Properties.field + "[\""; //$NON-NLS-1$
+            String propertyFieldTail = "\"]" + _SUFFIX_USER_VAR; //$NON-NLS-1$
+            boolean propPattern = userVarValue.startsWith(propertyFieldHead) && userVarValue.endsWith(propertyFieldTail)
+                    && userVarValue.length() > propertyFieldHead.length() + propertyFieldTail.length();
+            if (!validUserVars.contains(userVarValue) && !propPattern) {
+                return false;
+            }
         }
 
         return true;
