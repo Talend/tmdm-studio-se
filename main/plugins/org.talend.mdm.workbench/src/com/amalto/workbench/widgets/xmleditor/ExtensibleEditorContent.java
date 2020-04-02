f9bd0134614b2e0c1c8034afd1310263bb8df0d3
sbliu <sbliu@talend.com>
2020-04-02 10:10:36 +0800

    TMDM-14523 Hide password in source editor.

--- main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/widgets/xmleditor/ExtensibleEditorContent.java
+++ main/plugins/org.talend.mdm.workbench/src/com/amalto/workbench/widgets/xmleditor/ExtensibleEditorContent.java
@@ -14,7 +14,17 @@ package com.amalto.workbench.widgets.xmleditor;
 
 public class ExtensibleEditorContent {
 
+    private static final String TAG_PASSWORD_GEGIN = "<password>";
+
+    private static final String TAG_PASSWORD_END = "</password>";
+
+    private static final String MASKCODE_CHAR = "*";
+
+    private static final String MASKCODE = "*******";
+
     protected String content = "";//$NON-NLS-1$
+
+    protected String maskedContent = "";//$NON-NLS-1$
 
     public ExtensibleEditorContent(String content) {
 
@@ -26,11 +36,66 @@ public class ExtensibleEditorContent {
         return content;
     }
 
-    public void setContent(String content) {
+    public String getMaskContent() {
+        return maskedContent;
+    }
 
-        if (content == null)
+    public void setContent(String newContent) {
+        if (newContent == null) {
             this.content = "";//$NON-NLS-1$
-        else
-            this.content = content;
+            this.maskedContent = "";
+        } else {
+            if (isPasswordHiden(newContent)) {
+                if (contentChanged(newContent)) {
+                    String[] splitNewContent = splitbyPasswordTag(newContent);
+                    String[] splitContent = splitbyPasswordTag(content);
+                    this.content = splitNewContent[0] + splitContent[1] + splitNewContent[2];
+                    this.maskedContent = hidePassword(content);
+                }
+            } else {
+                this.content = newContent;
+                this.maskedContent = hidePassword(content);
+            }
+        }
+    }
+
+    private String hidePassword(final String content) {
+        String[] splitbyPasswordTag = splitbyPasswordTag(content);
+        if (splitbyPasswordTag != null) {
+            return splitbyPasswordTag[0] + (TAG_PASSWORD_GEGIN + MASKCODE + TAG_PASSWORD_END) + splitbyPasswordTag[2];
+        }
+
+        return content;
+    }
+
+    private boolean isPasswordHiden(final String content) {
+        String[] splitbyPasswordTag = splitbyPasswordTag(content);
+        if (splitbyPasswordTag != null) {
+            return splitbyPasswordTag[1].contains(MASKCODE_CHAR);
+        }
+
+        return false;
+    }
+
+    private boolean contentChanged(final String newcontent) {
+        String[] splitbyPassword_newcontent = splitbyPasswordTag(newcontent);
+        String[] splitbyPassword_maskcontent = splitbyPasswordTag(maskedContent);
+
+        if(splitbyPassword_newcontent != null && splitbyPassword_maskcontent != null) {
+            return !(splitbyPassword_newcontent[0].equals(splitbyPassword_maskcontent[0])
+                    && splitbyPassword_newcontent[2].equals(splitbyPassword_maskcontent[2]));
+        }
+        
+        return true;
+    }
+
+    private String[] splitbyPasswordTag(String _content) {
+        int start = _content.indexOf(TAG_PASSWORD_GEGIN);
+        int end = _content.indexOf(TAG_PASSWORD_END) + TAG_PASSWORD_END.length();
+        if (start != -1) {
+            return new String[] { _content.substring(0, start), _content.substring(start, end), _content.substring(end) };
+        }
+
+        return null;
     }
 }
